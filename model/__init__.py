7224a4378b7d3de1aaeb54da7ae9cc8416539bc1
007 <007lookworld@gmail.com>
2025-10-29 20:21:26 +0800

    refactor(model): 重构Word2Vec模型文件结构并添加GloVe文档
    将SkipGram和CBOW模型移至word2vec子目录
    新增GloVe技术文档，详细解释其原理和实现

--- model/__init__.py
+++ model/__init__.py
@@ -1,5 +1,5 @@
-from .skip_gram import SkipGramModel
-from .cbow import CBOWModel
+from .word2vec.skip_gram import SkipGramModel
+from .word2vec.cbow import CBOWModel
 
 __all__ = [
     'SkipGramModel', 
