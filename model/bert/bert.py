42f63c30c790509e125674bc388a5ad178ff891e
007 <007lookworld@gmail.com>
2025-10-31 18:17:16 +0800

    docs(model): 添加BERT模型的基本介绍文档
    添加BERT模型的基本概念和原理说明文档，包括其全称、作用及两个主要预训练任务

---
+++ model/bert/bert.py
@@ -0,0 +1,7 @@
+# bert全称：Bidirectional Encoder Representations from Transformers 来自Transformers的双向编码器
+# 作用：将输入的文本序列转换为向量表示，用于下游任务
+
+# BERT 是一种基于 Transformer 架构的预训练语言模型，它在预训练时使用了两个主要任务：
+
+# 掩码语言建模（Masked Language Modeling, MLM）
+# 下一句预测（Next Sentence Prediction, NSP）
